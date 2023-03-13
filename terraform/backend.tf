48153a4127d0c1513b58a317c6167c8653e4564c
Chihane Sofiane <chihanesofiane@gmail.com>
2023-03-13 16:21:48 +0100

    backendcommit1621

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "projet-c-deploy-app-eks"
-    region = "us-east-1"
-    key = "eks/terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "projet-c-deploy-app-eks"
+#     region = "us-east-1"
+#     key = "eks/terraform.tfstate"
+#   }
+# }
